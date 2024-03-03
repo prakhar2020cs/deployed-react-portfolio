cf5d4922eb4fb0b31edceec995e754bce0bc2735
Prakhar Sharma <Prakhar06035@gmail.com>
2024-03-03 18:23:41 +0530

    project component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero/Hero.jsx'
 import styles from  './App.module.css';
 import Navbar from './components/Navbar/Navbar.jsx';
+import { Skills } from './components/Skills/Skills.jsx';
 //import About from './components/About/About.jsx';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className={styles.App}>
  <Navbar/>
   <Hero/>
+  <Skills/>
 
  {/*<About/>*/}
   </div>
--- vite-project/src/components/Skills/Skills.jsx
+++ vite-project/src/components/Skills/Skills.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import skills from '../../data/skills.json';
+import { getImageUrl } from '../../utils';
 
 export const Skills = () => {
   return (
     <section id="skills">
         <h2>Skills</h2>
         <div>
-            <div>
-                {skills.map((skill,id)=>{
-                    return(
-                        <div key={id}>
-                            <div>
-                                <img src={getImageUrl(skill.imagesrc) alt={skill.name}}></img>
-                                <p>{skill.title}</p>
-                            </div>
-                            
-                            
-                        
-                    );}
-            
-                </div>
-            
+            <div>{
+                skills.map((skill,id)=>{
+                   return (
+                   <div key={id}>
+                    <div> <img src={getImageUrl(skills.imageSrc)} alt={skills.title} /> </div>
+                    <p>{skills.title}</p>
+                   </div>
+                   );
+
+                })
+                
+                };</div>
+           
         </div>
+        
+        
     </section>
-  )
-}
+  );
+};
